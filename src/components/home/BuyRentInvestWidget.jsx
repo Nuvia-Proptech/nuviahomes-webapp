5e9a9d6092eeb8ade094151c25b01d127e8bbad7
Ezewi Osorachukwu <oezewi@gmail.com>
2025-09-20 09:24:58 +0100

    ...

--- src/components/home/BuyRentInvestWidget.jsx
+++ src/components/home/BuyRentInvestWidget.jsx
@@ -8,7 +8,7 @@ import searchIcon from "@/assets/icons/search-icon.svg";
 
 export default function BuyRentInvestWidget({widgetStyle, searchText}) {
   return (
-    <div className={`${widgetStyle}`}>
+    <div className={`${widgetStyle} `}>
       <div className="flex justify-center z-50 -mb-3 ">
         {["Buy", "Rent", "Invest"].map((item) => (
           <div
@@ -20,8 +20,8 @@ export default function BuyRentInvestWidget({widgetStyle, searchText}) {
         ))}
       </div>
       {/*  */}
-      <div className="skewed overflow-hidden flex justify-between text-lg text-black max-w-7xl mx-auto z-50">
-        <div className="border-r border-white/35 bg-[#DFE6E7] h-[65px] w-full flex flex-col justify-center px-2">
+      <div className="skewed overflow-hidden flex justify-between text-lg text-black max-w-[95rem] mx-auto z-50">
+        <div className="widget-tab-style border-r border-white/35 h-[65px] w-full flex flex-col justify-center px-2">
           <p className="unskewed text-2">Location</p>
           <div className="unskewed flex text-12 justify-between">
             <p className="">Choose what you are looking at</p>
