15c1057ef110dddad786f7cac20281d5254a3a79
Daniel Chukwudubem <princedaniel7890@gmail.com>
2025-10-16 14:10:43 +0100

    fix(): made the footer to exist in some dynamic pages

--- src/components/shared/footer/ConditionalFooter.jsx
+++ src/components/shared/footer/ConditionalFooter.jsx
@@ -4,8 +4,12 @@ import Footer from "@/components/shared/footer/Footer";
 
 export function ConditionalFooter() {
   const pathname = usePathname();
+
+  
   const footerRoutes = ["/", "/who-we-are", "/listing", "/agents", "/our-blog", "/contact"];
-  const showFooter = footerRoutes.includes(pathname);
+
+  // Show footer if the path matches or starts with one of the routes
+  const showFooter = footerRoutes.some((route) => pathname.startsWith(route));
 
   return showFooter ? <Footer /> : null;
 }
