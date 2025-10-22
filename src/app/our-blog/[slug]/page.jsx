2a3c69148dece8ef13d23915162015ae71c14f3c
Daniel Chukwudubem <princedaniel7890@gmail.com>
2025-10-22 20:30:58 +0100

    fix(): enhanced the blog

--- src/app/our-blog/[slug]/page.jsx
+++ src/app/our-blog/[slug]/page.jsx
@@ -7,6 +7,7 @@ import TrendingBlog from "@/components/blog/blogArticles/TrendingBlog";
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import { Quote } from "@/components/shared/Icons/Quote";
 
 export default async function BlogDetailPage({ params }) {
   const { slug } = await params;
@@ -46,9 +47,29 @@ export default async function BlogDetailPage({ params }) {
                   </p>
                 ))}
               </div>
-              <div className="flex justify-between mt-8">
+              <div className="gradient-border my-4">
+                <div className="gradient-border-inner">
+                  <div className=" flex md:space-x-4 px-8 py-4 md:gap-6">
+                    <span className="pt-2">
+                      <Quote />
+                    </span>
+                    <div>
+                    <p>
+              Buying your first home is more than just a financial investment — it’s a deeply personal journey toward building a space that reflects your life, your goals, and your future; a place where you’ll feel secure, grow through new experiences, and create memories that last a lifetime.
+                    </p>
+                      <p className="mt-3 font-bold"> By {blog.author}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               {blog.content.map((paragraph, index) => (
+                  <p key={index} className="text-16">
+                    {paragraph}
+                  </p>
+                ))}
+              <div className="flex justify-between mt-8 md:mt-10">
                 <div className="space-y-2 md:space-y-4">
-                  <p className="text-16 font-semibold">Related Tags</p>
+                  <p className="text-xl font-semibold">Related Tags</p>
                   <div className="flex gap-2 align-middle">
                     {blog.tags.map((tag, index) => (
                       <button
@@ -61,7 +82,7 @@ export default async function BlogDetailPage({ params }) {
                   </div>
                 </div>
                 <div className="space-y-2 md:space-y-4">
-                  <p className="text-16 font-semibold">Social Share</p>
+                  <p className="text-xl font-semibold">Social Share</p>
                   <div className="flex gap-2 md:gap-6">
                     <button className="text-16 hover:opacity-80 transition-opacity">
                       <FaFacebook className="w-10 h-10 text-[#1877F2]" />
@@ -87,9 +108,9 @@ export default async function BlogDetailPage({ params }) {
                         <button className="text-16   cursor-pointer">
                           Prev Post
                         </button>
-                      <p className="text-base font-semibold">
-                        {BlogData.find((b) => b.id === blog.id - 1)?.title}
-                      </p>
+                        <p className="text-base font-semibold">
+                          {BlogData.find((b) => b.id === blog.id - 1)?.title}
+                        </p>
                       </Link>
                     </>
                   )}
@@ -97,13 +118,16 @@ export default async function BlogDetailPage({ params }) {
                 <div className="text-right">
                   {blog.id < BlogData.length && (
                     <>
-                      <Link href={`/our-blog/${blog.id + 1}`} className="block ">
+                      <Link
+                        href={`/our-blog/${blog.id + 1}`}
+                        className="block "
+                      >
                         <button className="text-16 cursor-pointer">
                           Next Post
                         </button>
-                      <p className="text-base font-semibold">
-                        {BlogData.find((b) => b.id === blog.id + 1)?.title}
-                      </p>
+                        <p className="text-base font-semibold">
+                          {BlogData.find((b) => b.id === blog.id + 1)?.title}
+                        </p>
                       </Link>
                     </>
                   )}
---
+++ src/components/shared/Icons/Quote.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Quote = () => {
+  return (
+    <svg
+      width="18"
+      height="15"
+      viewBox="0 0 18 15"
+      fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          
+    >
+      <path
+        d="M17.3359 14.616L13.6399 -5.14984e-05H9.77587L11.5119 14.616H17.3359ZM7.55993 14.616L5.82393 -5.14984e-05H-6.84261e-05L3.69593 14.616H7.55993Z"
+        fill="url(#paint0_linear_687_6334)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_687_6334"
+          x1="16.4755"
+          y1="-5.27064"
+          x2="-5.35364"
+          y2="-1.95485"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#1B5590" />
+          <stop offset="0.495192" stopColor="#35AF72" />
+          <stop offset="1" stop-color="#B5E03A" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
