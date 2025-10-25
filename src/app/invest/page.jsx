41362a199681f618d36b90ee8dc40c9163775fa1
Daniel Chukwudubem <princedaniel7890@gmail.com>
2025-10-25 21:58:24 +0100

    feat() started the invest page

--- src/app/invest/page.jsx
+++ src/app/invest/page.jsx
@@ -1,54 +1,14 @@
 import React from "react";
-import Nav from "@/components/shared/Nav";
+import { InvestDescription } from "@/components/invest/InvestDescription";
 import AltHero from "@/components/shared/AltHero";
+import { HowInvestmentWorks } from "@/components/invest/HowInvestmentWorks";
 
 export default function InvestPage() {
   return (
     <section>
       <AltHero pageName=" Invest" />
-      <div>
-        <div className="flex">
-          <div className="w-1/2">
-            <p>Unlock a World of Real Estate Investment</p>
-          </div>
-          <div className="w-1/2">
-            <p>
-              Best for those who wants a balance of good returns and medium,
-              risk. Our Plans invest in Rented buildings and properties all over
-              West Africa and returns 20 - 30 Percent per annum
-            </p>
-
-            <div>
-              <p>Expected Outcomes</p>
-              <p>
-                Our real estate plans is invested into a portfolio of properties
-                across west Africa. Your funds are pooled and used to purchase a
-                property which is then rented out and managed by experts to
-                generate returns for you
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* Belt */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          <div>
-            <p>20-30%</p>
-            <p>Expected Returns</p>
-          </div>
-          <div>
-            <p>Medium</p>
-            <p>Risk Level</p>
-          </div>
-          <div>
-            <p>100%</p>
-            <p>Capital Guranteed</p>
-          </div>
-          <div>
-            <p>0%</p>
-            <p>Return Gurateed</p>
-          </div>
-        </div>
-      </div>
+      <InvestDescription />
+      <HowInvestmentWorks/>
     </section>
   );
 }
---
+++ src/components/shared/Icons/InvestLogo.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const InvestLogo = () => {
+  return (
+    <div>
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M36 36H12V12L36 36Z" fill="#DFE6E7" />
+        <path
+          d="M48 0H0L12 12L36 12L36 36L48 48V0Z"
+          fill="url(#paint0_linear_618_2079)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_618_2079"
+            x1="5.80645"
+            y1="5.64705"
+            x2="47.8443"
+            y2="25.1074"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#1B5590" />
+            <stop offset="0.495192" stop-color="#35AF72" />
+            <stop offset="1" stop-color="#B5E03A" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+};
